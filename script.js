d36d3b101cae7d16d93a73df64c6937c65dd2b95
NyanLight <128150876+NyanLight@users.noreply.github.com>
2024-01-07 13:52:11 +0400

    make separate createCard function a method of Game object

--- script.js
+++ script.js
@@ -1,9 +1,33 @@
 const main = document.querySelector('main');
 
+function Game(title, hours, beat) {
+    this.title = title;
+    this.hours = hours;
+    this.beat = beat;
+    this.createCard = function() {
+        const card = document.createElement('div');
+        card.classList.add("card");
+        const titleCard = document.createElement('p');
+        card.appendChild(titleCard);
+        titleCard.textContent = `${this.title}`;
+        const hoursCard = document.createElement('p');
+        card.appendChild(hoursCard);
+        hoursCard.textContent = `${this.hours}`;
+        const beatCard = document.createElement('p');
+        card.appendChild(beatCard);
+        if (this.beat === true) {
+            beatCard.textContent = "Completed";
+        } else {
+            beatCard.textContent = "Not completed";
+        };
+        main.appendChild(card);
+    }
+};
+
 const fable =  {
     title: "Fable 2",
     hours: 35,
-    beat: true, 
+    beat: true,
 };
 
 const fallout = {
@@ -14,27 +38,10 @@ const fallout = {
 
 let myGames = [fable, fallout];
 
-function Game(title, hours, beat) {
-    this.title = title;
-    this.hours = hours;
-    this.beat = beat;
-};
-
 function addGame(game) {
     myGames.push(game);
 };
 
-function createCard() {
-    const card = document.createElement('div');
-    card.classList.add("card");
-    const title = document.createElement('p');
-    card.appendChild(title);
-    const hours = document.createElement('p');
-    card.appendChild(hours);
-    const beat = document.createElement('p');
-    card.appendChild(beat);
-    main.appendChild(card);
-}
 
 function displayLibrary() {
     for (const game of myGames) {
