9d09a41b48b7d2d82daaf447542af4389542878e
NyanLight <128150876+NyanLight@users.noreply.github.com>
2024-01-10 20:01:04 +0400

    add test version of remove btn logic and format the code

--- script.js
+++ script.js
@@ -1,58 +1,64 @@
-const cardSection = document.getElementById('cardSection');
-const dialog = document.querySelector('dialog');
-
-function Game(title, hours, beat) {
-    this.title = title;
-    this.hours = hours;
-    this.beat = beat;
-    this.createCard = function() {
-        const card = document.createElement('div');
-        card.classList.add("card");
-        const titleCard = document.createElement('p');
-        card.appendChild(titleCard);
-        titleCard.textContent = `${this.title}`;
-        const hoursCard = document.createElement('p');
-        card.appendChild(hoursCard);
-        hoursCard.textContent = `${this.hours}`;
-        const beatCard = document.createElement('p');
-        card.appendChild(beatCard);
-        if (this.beat === true) {
-            beatCard.textContent = "Completed";
-        } else {
-            beatCard.textContent = "Not completed";
-        };
-        const removeBtn = document.createElement('button')
-        removeBtn.classList.add("removeBtn");
-        removeBtn.textContent = "Delete";
-        card.appendChild(removeBtn);
-        cardSection.appendChild(card);
-    }
-};
+const cardSection = document.getElementById("cardSection");
+const dialog = document.querySelector("dialog");
 
 let myGames = [];
+function Game(title, hours, beat) {
+  this.title = title;
+  this.hours = hours;
+  this.beat = beat;
+  this.index = myGames.length;
+  this.createCard = function () {
+    const card = document.createElement("div");
+    card.setAttribute("data-index", this.index);
+    card.classList.add("card");
+    const titleCard = document.createElement("p");
+    card.appendChild(titleCard);
+    titleCard.textContent = `${this.title}`;
+    const hoursCard = document.createElement("p");
+    card.appendChild(hoursCard);
+    hoursCard.textContent = `${this.hours}`;
+    const beatCard = document.createElement("p");
+    card.appendChild(beatCard);
+    if (this.beat === true) {
+      beatCard.textContent = "Completed";
+    } else {
+      beatCard.textContent = "Not completed";
+    }
+    const removeBtn = document.createElement("button");
+    removeBtn.classList.add("removeBtn");
+    removeBtn.textContent = "Delete";
+    removeBtn.addEventListener("click", () => {
+      myGames.splice(this.index, 1);
+      displayLibrary;
+    });
+    card.appendChild(removeBtn);
+    cardSection.appendChild(card);
+  };
+}
 
 function addGame(game) {
-    myGames.push(game);
-};
+  myGames.push(game);
+}
 
 
 function displayLibrary() {
-    cardSection.innerHTML = "";
-    for (const game of myGames) {
-        game.createCard();
-    };
-};
+  cardSection.innerHTML = "";
+  for (const game of myGames) {
+    game.createCard();
+  }
+}
 
-const dialogBtn = document.getElementById('dialogBtn');
-dialogBtn.addEventListener('click', () => dialog.showModal());
+const dialogBtn = document.getElementById("dialogBtn");
+dialogBtn.addEventListener("click", () => dialog.showModal());
 
 const addBtn = document.getElementById("addBtn");
 addBtn.addEventListener("click", (e) => {
-    e.preventDefault();
-    const titleInput = document.getElementById('titleInput').value;
-    const hoursInput = document.getElementById('hoursInput').value;
-    const beatChecked = (document.querySelector(`input[name="beat"]:checked`).value === 'true');
-    const game = new Game(titleInput, Number(hoursInput), beatChecked);
-    addGame(game);
-    displayLibrary(); 
-})
+  e.preventDefault();
+  const titleInput = document.getElementById("titleInput").value;
+  const hoursInput = document.getElementById("hoursInput").value;
+  const beatChecked =
+    document.querySelector(`input[name="beat"]:checked`).value === "true";
+  const game = new Game(titleInput, Number(hoursInput), beatChecked);
+  addGame(game);
+  displayLibrary();
+});
